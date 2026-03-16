516f7e1f8857648aae751fd45ebb4e4cb84cee05
mackun-ui <m.ackun@alustudent.com>
2026-03-16 09:32:04 +0000

    preventing multiple Prisma clients fixed

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,15 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import pkg from "pg"
 
-const { Pool } = pkg
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-})
-
-const adapter = new PrismaPg(pool)
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  global.prisma ||
   new PrismaClient({
-    adapter
+    log: ["error", "warn"]
   })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
+  global.prisma = prisma
 }
